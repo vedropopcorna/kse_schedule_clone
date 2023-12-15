1cbc7c5c1ceb5f65a35d8d35d5ccb6053a725d79
Eduard Pekach <epekach@kse.org.ua>
2023-12-15 15:15:30 +0200

    Added files

--- utils/data.py
+++ utils/data.py
@@ -13,14 +13,17 @@ class DataManager:
         self.input_groups_df = pd.read_csv(f'{data_path}/groups.csv').map(strip_whitespace)
         self.input_students_df = pd.read_csv(f'{data_path}/students.csv').map(strip_whitespace)
         self.input_lessons_df = pd.read_csv(f'{data_path}/lessons.csv').map(strip_whitespace)
-        self.input_teachers_df = pd.read_csv(f'{data_path}/teachers.csv')    
-    
+        self.input_teachers_df = pd.read_csv(f'{data_path}/teachers.csv')
+
         self.group_to_pupils = {}
         self.group_to_id = {}
+        self.group_id = {}
         self.group_intersection = {}
         self.teachers_availability = None
+        self.teacher_id = {}
         self.existing_schedule_df = existing_schedule_df
         self.existing_schedule_records = {}
+        self.teacher_online_availability = {}
 
         self.preprocessing()
 
@@ -33,26 +36,26 @@ class DataManager:
                 'time_slot_id': row['time_slot_id'],
                 'room_id': row['room_id']
             }
-            
 
     def preprocessing(self):
         self._preprocess_audiences()
-        self._preprocess_teachers()
         self._preprocess_groups()
         self._preprocess_students()
         self._preprocess_lessons()
-        
-        
+        self._preprocess_teachers()
 
     def _preprocess_audiences(self):
         self.input_audiences_df = self.input_audiences_df[~pd.isna(self.input_audiences_df['is_shelter_id'])].copy()
-        self.input_audiences_df.loc[:, 'name'] = self.input_audiences_df.apply(lambda row: f"{row['id']}_{row['name']}", axis=1)
+        self.input_audiences_df.loc[:, 'name'] = self.input_audiences_df.apply(lambda row: f"{row['id']}_{row['name']}",
+                                                                               axis=1)
         self.input_audiences_df = self.input_audiences_df.rename(columns={'id': 'kse_id'})
+        self.input_audiences_df['is_online'] = np.where(self.input_audiences_df['is_shelter_id'] == 0, 1, 0)
         self.input_audiences_df['id'] = np.arange(self.input_audiences_df.shape[0])
-        self.input_audiences_df['capacity'] = self.input_audiences_df.apply(lambda row: row['capacity'] + 50 if row['name'] == '1003_TA Ventures Classroom' else row['capacity'], axis=1)
+        self.input_audiences_df['capacity'] = self.input_audiences_df.apply(
+            lambda row: row['capacity'] + 50 if row['name'] == '1003_TA Ventures Classroom' else row['capacity'],
+            axis=1)
         self.input_audiences_df['capacity'] = self.input_audiences_df['capacity'] + 10  # we just need more space :)
-
-
+        print(self.input_audiences_df.head(30))
     def _preprocess_students(self):
         self.input_students_df['id'] = np.arange(self.input_students_df.shape[0])
 
@@ -60,7 +63,7 @@ class DataManager:
             self.input_students_df[c] = self.input_students_df[c].astype(str).str.strip()
 
         self.input_students_df = self.input_students_df.replace('nan', np.nan)
-        self.input_students_df['name'] = self.input_students_df['Прізвище'] + ' ' +  self.input_students_df["Ім'я"]
+        self.input_students_df['name'] = self.input_students_df['Прізвище'] + ' ' + self.input_students_df["Ім'я"]
 
     def _preprocess_groups(self):
         self.group_intersection = get_group_intersections(self.input_students_df)
@@ -70,93 +73,133 @@ class DataManager:
             d = {k.rstrip().lstrip(): v for k, v in d.items()}
             self.group_to_pupils.update(d)
 
-        self.group_to_id = {k: num for num, (k,v) in enumerate(self.group_to_pupils.items())}
+        self.group_to_id = {k: num for num, (k, v) in enumerate(self.group_to_pupils.items())}
+        self.input_groups_df['name'] = self.input_groups_df['name'].str.strip()
+        self.group_id = dict(zip(self.input_groups_df['name'], self.input_groups_df['id']))
+        # print(self.group_to_pupils)
 
     def _preprocess_lessons(self):
         # TODO: add support of online lections
-        self.input_lessons_df = self.input_lessons_df[self.input_lessons_df['format'] == 'офлайн']
+        # self.input_lessons_df = self.input_lessons_df[self.input_lessons_df['format'] == 'офлайн']
+        self.input_lessons_df['is_online'] = np.where(self.input_lessons_df['format'] == 'офлайн', 0, 1)
+        print(sum(self.input_audiences_df['is_online']))
+        # online_room_id = self.input_audiences_df[self.input_audiences_df['is_online'] == 1]['id'].iloc[0]
+        # print(online_room_id)
+        # self.input_lessons_df['assigned_room_id'] = np.where(self.input_lessons_df['is_online'] == 1,
+        #                                                      online_room_id,
+        #                                                      np.nan)
         self.input_lessons_df['count'] = self.input_lessons_df['count'].astype(int)
-        duplicated_rows = pd.DataFrame(self.input_lessons_df.loc[self.input_lessons_df.index.repeat(self.input_lessons_df['count'])].reset_index(drop=True))
+        duplicated_rows = pd.DataFrame(
+            self.input_lessons_df.loc[self.input_lessons_df.index.repeat(self.input_lessons_df['count'])].reset_index(
+                drop=True))
         duplicated_rows.drop('count', axis=1, inplace=True)
         self.input_lessons_df = duplicated_rows.copy()
+        self.input_lessons_df = self.input_lessons_df.rename(columns={'id': 'kse_id'})
         self.input_lessons_df['id'] = np.arange(self.input_lessons_df.shape[0])
 
-
-        self.input_lessons_df['pupils'] = self.input_lessons_df['group'].apply(lambda v: self.group_to_pupils.get(v, -1))
+        self.input_lessons_df['pupils'] = self.input_lessons_df['group'].apply(
+            lambda v: self.group_to_pupils.get(v, -1))
         logger.debug(f'Before filtering lessons count: {self.input_lessons_df.shape[0]}')
-        logger.warning(f"This groups doesnt have pupils: {self.input_lessons_df[self.input_lessons_df['pupils'] == -1]['group'].values.tolist()}")
+        logger.warning(
+            f"This groups doesnt have pupils: {self.input_lessons_df[self.input_lessons_df['pupils'] == -1]['group'].values.tolist()}")
         self.input_lessons_df = self.input_lessons_df[self.input_lessons_df['pupils'] != - 1]
         logger.debug(f'After filtering lessons count: {self.input_lessons_df.shape[0]}')
-        
+        # print(self.input_lessons_df)
 
     def _preprocess_teachers(self):
         timeslot_list = get_timeslot_list()
         timeslot_dict = {day: [] for day in ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY']}
         for timeslot in timeslot_list:
             timeslot_dict[timeslot.day_of_week].append(timeslot)
-            
+
         self.input_teachers_df['name'] = self.input_teachers_df['name'].apply(lambda v: v.rstrip().lstrip())
+
+        for _, row in self.input_teachers_df.iterrows():
+            teacher_name = row['name'].strip()
+            online_availability = {
+                'MONDAY': str(row['monday']).strip() == 'online',  # Assuming you have columns like 'monday_online' for each day
+                'TUESDAY': str(row['tuesday']).strip() == 'online',
+                'WEDNESDAY': str(row['wednesday']).strip() == 'online',
+                'THURSDAY': str(row['thursday']).strip() == 'online',
+                'FRIDAY': str(row['friday']).strip() == 'online'
+            }
+            num = 0
+            self.teacher_online_availability[teacher_name] = {}
+            for day in ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY']:
+                for i in range(6):
+                    self.teacher_online_availability[teacher_name][num] = 1 if online_availability[day] else 0
+                    num += 1
         self.input_teachers_df = self.input_teachers_df.replace('online ', 1).fillna(1)
         self.teachers_availability = get_teacher_availability(self.input_teachers_df, timeslot_dict)
-
+        self.teachers_id = dict(zip(self.input_teachers_df['name'], self.input_teachers_df['id']))
 
     def generate_optapy_problem(self, reschedule_lesson_id=None, forbidden_time_slots=None):
         timeslot_list = get_timeslot_list()
 
         room_list = []
         for _, row in self.input_audiences_df.iterrows():
-            room_list.append(Room(row['id'], row['name'], row['capacity']))
+            room_list.append(Room(row['id'], row['kse_id'], row['name'], row['is_online'], row['capacity']))
 
         group_objects = {}
         for group_name, pupils in self.group_to_pupils.items():
-            group_id = self.group_to_id[group_name]
-            group_objects[group_name] = StudentGroup(group_id, group_name, pupils)
-
+            group_num = self.group_to_id[group_name]
+            group_id = self.group_id[group_name]
+            group_objects[group_name] = StudentGroup(group_num, group_id, group_name, pupils)
 
         lesson_list = []
 
         for num, (_, row) in enumerate(self.input_lessons_df.iterrows()):
             try:
-                students_df = self.input_students_df[self.input_students_df[row['subject'].strip()] ==  row['group']].copy()
+                # print(row)
+                teacher_id = int(self.teachers_id[row['teacher']])
+                # print(teacher_id)
+                students_df = self.input_students_df[
+                    self.input_students_df[row['subject'].strip()] == row['group']].copy()
 
-                group = group_objects[row['group']] #StudentGroup(group_to_id[row['group']], row['group'], students_df.shape[0])
-                
+                group = group_objects[
+                    row['group']]  # StudentGroup(group_to_id[row['group']], row['group'], students_df.shape[0])
                 if num == reschedule_lesson_id:
-                    #print('reschedule_lesson_id', reschedule_lesson_id)
-                    lesson = Lesson(row['id'], row['subject'], 
-                                    Teacher(row['teacher'], self.teachers_availability[row['teacher']]),
-                                    group, students_df.shape[0], group_intersection=self.group_intersection,
-                                    forbidden_timeslots={int(k): int(v) for k, v in forbidden_time_slots.items()}, is_fixed=False)
+                    print('reschedule_lesson_id', reschedule_lesson_id)
+                    lesson = Lesson(row['id'], row['kse_id'], row['subject'],
+                                    Teacher(row['teacher'], self.teacher_online_availability[row['teacher']], self.teachers_availability[row['teacher']]), teacher_id,
+                                    row['is_lection'], group, students_df.shape[0],
+                                    row['is_online'], group_intersection=self.group_intersection,
+                                    forbidden_timeslots={int(k): int(v) for k, v in forbidden_time_slots.items()},
+                                    is_fixed=False)
 
                 elif row['id'] in self.existing_schedule_records:
-                    #print('in existing_schedule_records', num)
+                    print('in existing_schedule_records', num)
                     ideal_time_slot_id = self.existing_schedule_records[row['id']]['time_slot_id']
                     ideal_room_id = self.existing_schedule_records[row['id']]['room_id']
-                    lesson = Lesson(row['id'], row['subject'],
-                            Teacher(row['teacher'], self.teachers_availability[row['teacher']]),
-                            group, students_df.shape[0], group_intersection=self.group_intersection, 
-                            ideal_room_id=ideal_room_id, ideal_timeslot_id=ideal_time_slot_id, is_fixed=True)
+                    lesson = Lesson(row['id'], row['kse_id'], row['subject'],
+                                    Teacher(row['teacher'], self.teacher_online_availability[row['teacher']], self.teachers_availability[row['teacher']]), teacher_id,
+                                    row['is_lection'], group, students_df.shape[0],
+                                    row['is_online'], group_intersection=self.group_intersection,
+                                    ideal_room_id=ideal_room_id, ideal_timeslot_id=ideal_time_slot_id, is_fixed=True)
 
                 else:
-                    lesson = Lesson(row['id'], row['subject'],
-                                    Teacher(row['teacher'], self.teachers_availability[row['teacher']]),
-                                    group, students_df.shape[0], group_intersection=self.group_intersection)
-                #print(lesson)
+                    lesson = Lesson(row['id'], row['kse_id'], row['subject'],
+                                    Teacher(row['teacher'], self.teacher_online_availability[row['teacher']], self.teachers_availability[row['teacher']]), teacher_id,
+                                    row['is_lection'], group, students_df.shape[0], row['is_online'],
+                                    group_intersection=self.group_intersection)
+                # print('lesson: ', lesson)
                 lesson_list.append(lesson)
+                # print('lesson: ', lesson_list)
             except Exception as e:
                 print('exception', e)
+            #     break
 
         lesson = lesson_list[0]
         lesson.is_pinned = True
         lesson.set_timeslot(timeslot_list[0])
         lesson.set_room(room_list[0])
-        
+
         lesson_list[0] = lesson
         return TimeTable(timeslot_list, room_list, lesson_list)
 
+
 if __name__ == '__main__':
     data_manager = DataManager('uploaded_files')
-    
 
     import pdb
     from constraints import define_constraints, get_solver_config
@@ -171,5 +214,4 @@ if __name__ == '__main__':
     explanation = score_manager.explainScore(solution)
 
     logger.debug(f"Final score: {str(solution.get_score())}")
-    logger.debug(f"Score explenation: {str(explanation)}")
-
+    logger.debug(f"Score explanation: {str(explanation)}")
--- utils/schedule.py
+++ utils/schedule.py
@@ -1,48 +1,170 @@
 import pandas as pd
-from .time_utils import get_timeslot_list
+import numpy as np
+from datetime import timedelta, datetime
+import json
+# from pandas import json_normalize
+# from .time_utils import get_timeslot_list
+
+
+def _get_pair_number(start_time_str):
+    pair_times = [
+        ('08:30', '09:50', 1),
+        ('10:00', '11:20', 2),
+        ('11:30', '12:50', 3),
+        ('13:30', '14:50', 4),
+        ('15:00', '16:20', 5),
+        ('16:30', '17:50', 6),
+        ('18:00', '19:20', 7),
+        ('19:30', '20:50', 8)
+    ]
+
+    formatted_start_time = str(start_time_str).strip()[:5]
+
+    start_time = datetime.strptime(formatted_start_time, '%H:%M')
+
+    for start, end, pair_number in pair_times:
+        start_dt = datetime.strptime(start, '%H:%M')
+        end_dt = datetime.strptime(end, '%H:%M')
+        if start_dt <= start_time < end_dt:
+            return pair_number
+
+    return None
+
 
 class ScheduleManager:
 
-    def __init__(self, optapy_solution=None, raw_schedule_df=None):
-        if optapy_solution != None:
+    def __init__(self, optapy_solution=None, raw_schedule_df=None, start_date=None):
+        self.start_date = start_date
+        self.id_shed = 2
+        if optapy_solution is not None:
             self.raw_schedule_df = self._parse_optapy_solution(optapy_solution)
         else:
             self.raw_schedule_df = raw_schedule_df
-    
+
     def _parse_optapy_solution(self, solution):
+        day_map = {
+            'MONDAY': 1,
+            'TUESDAY': 2,
+            'WEDNESDAY': 3,
+            'THURSDAY': 4,
+            'FRIDAY': 5,
+            'SATURDAY': 6,
+            'SUNDAY': 7
+        }
         scheduling_records_data = []
         for num, l in enumerate(solution.get_lesson_list()):
+            day_of_week = day_map.get(l.timeslot.day_of_week.upper(), 0)
+            lesson_date = self.start_date + timedelta(days=day_of_week)
             scheduling_records_data.append({
-                'room': f"{l.room.name} [{l.room.capacity}]" ,
-                'student_group': f"{l.student_group.name}", # [{l.student_group_capacity}]",
+                'room': f"{l.room.name} [{l.room.capacity}]",
+                'student_group': f"{l.student_group.name}",
+                'student_group_id': l.student_group.group_id,
+                # [{l.student_group_capacity}]",
+                'div': 6,
                 'subject': l.subject,
                 'teacher': ' '.join(l.teacher.name.split(' ')[:2]),
                 'day': l.timeslot.day_of_week,
+                'day_of_week': day_of_week,
+                'lesson_date': lesson_date,
                 'start_time': l.timeslot.start_time,
-                'lesson_id': l.id,
+                'num_pair': _get_pair_number(l.timeslot.start_time),
+                'lesson_id': l.lesson_id,
+                'is_online': l.is_online,
                 'room_id': l.room.id,
+                'auditory_id': l.room.auditory_id,
                 'time_slot_id': l.timeslot.id,
-                'schedule_id': num
+                'schedule_id': num,
+                'teacher_id': l.teacher_id,
+                'is_lection': l.is_lection
             })
-
+        # import pdb
+        # pdb.set_trace()
         raw_schedule_df = pd.DataFrame(scheduling_records_data)
         return raw_schedule_df
-    
-    def raw_schedule_to_pretty(self, raw_schedule_df):
-        raw_schedule_df['text'] = raw_schedule_df.apply(lambda row: f"{row['subject']}\n{row['student_group']}\n{row['teacher']}\n[{row['schedule_id']}]", axis=1)
-        pivot_df = raw_schedule_df.pivot(index=['day', 'start_time'], columns='room', values='text')
 
-        df = pivot_df.fillna('').copy()
-        df = df.reset_index()
+    def create_json_from_df(self, raw_schedule_df):
+        # print(raw_schedule_df['student_group_id'])
+        if not pd.api.types.is_datetime64_any_dtype(raw_schedule_df['lesson_date']):
+            raw_schedule_df['lesson_date'] = pd.to_datetime(raw_schedule_df['lesson_date'])
+
+        raw_schedule_df['lesson_date'] = raw_schedule_df['lesson_date'].dt.strftime('%Y-%m-%d')
+
+        for col in raw_schedule_df.select_dtypes(['datetime', 'timedelta']):
+            raw_schedule_df[col] = raw_schedule_df[col].apply(lambda x: x.strftime('%H:%M') if pd.notnull(x) else None)
+
+        mapping = {
+            'auditory_id': 'ID_AUD',
+            'div': 'ID_DIV',
+            'lesson_id': 'ID_DISC',
+            'is_lection': 'ID_STUD',
+            'teacher_id': 'ID_TEACH',
+            'num_pair': 'NUM_PAIR',
+            'lesson_date': 'DATE_PAIR',
+            'student_group_id': 'GROUPS'
+
+        }
+
+        df_mapped = raw_schedule_df.rename(columns=mapping)
+
+        df_mapped1 = df_mapped[['ID_AUD', 'ID_DIV']]
+        df_mapped1['ID_SHED'] = self.id_shed
+        df_mapped2 = df_mapped[['ID_DISC', 'ID_STUD', 'ID_TEACH', 'NUM_PAIR', 'DATE_PAIR', 'GROUPS']]
+        df_mapped = pd.concat([df_mapped1, df_mapped2], axis=1, join='outer')
+
+        df_mapped['ID_STUD'] = df_mapped['ID_STUD'].replace({0: 2})
+        df_mapped['GROUPS'] = df_mapped['GROUPS'].apply(lambda x: [x])
+
+        print(df_mapped)
+        json_data = df_mapped.to_json(orient='records')
+        # json_data = json.dumps(records, indent=2)
+
+        return json_data
+
+
+    def raw_schedule_to_pretty(self, raw_schedule_df):
+        raw_schedule_df['is_lection'] = np.where(self.raw_schedule_df['is_lection'] == 1,
+                                                             'лекція',
+                                                             'практика')
+        raw_schedule_df['is_online'] = np.where(self.raw_schedule_df['is_online'] == 1,
+                                                 'онлайн',
+                                                 'офлайн')
+        raw_schedule_df['text'] = raw_schedule_df.apply(
+            lambda row: f"{row['subject']}\n{row['student_group']}\n{row['teacher']}\n{row['is_lection']}\n{row['is_online']}\n[{row['schedule_id']}]", axis=1)
+        # print(raw_schedule_df.columns)
+        # print(raw_schedule_df[['day', 'start_time', 'day_of_week', 'num_pair', 'teacher_id', 'is_lection']])
+        # print(raw_schedule_df['start_time'].dtype)
+
+        raw_schedule_df['dup_id'] = raw_schedule_df.groupby(['day', 'start_time', 'room']).cumcount()
+        pivot_df = raw_schedule_df.set_index(['day', 'start_time', 'room', 'dup_id']).unstack(['room', 'dup_id'])
+
+        # raw_schedule_df = raw_schedule_df.drop_duplicates(['day', 'start_time', 'room'])
+        # print(raw_schedule_df.room.unique())
+
+        pivot_df.columns = [' '.join(map(str, col)).strip() for col in pivot_df.columns.values]
+        pivot_df = pivot_df.reset_index()
+        # pivot_df = raw_schedule_df.pivot(index=['day', 'start_time'], columns='room', values='text')
+
+
+        # df = pivot_df.fillna('').copy()
+        # df = df.reset_index()
+        # days_order = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY']
+        # df['day_int'] = df['day'].apply(lambda d: days_order.index(d))
+        # df.sort_values(by=['day_int', 'start_time'], inplace=True)
+        # df.drop('day_int', axis=1, inplace=True)
+
         days_order = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY']
-        df['day_int'] = df['day'].apply(lambda d: days_order.index(d))
-        df.sort_values(by=['day_int', 'start_time'], inplace=True)
-        df.drop('day_int', axis=1, inplace=True)
+        pivot_df['day_int'] = pivot_df['day'].apply(lambda d: days_order.index(d))
+        pivot_df.sort_values(by=['day_int', 'start_time'], inplace=True)
+        pivot_df.drop(['day_int'], axis=1, inplace=True)
+        columns_to_drop = [col for col in pivot_df.columns if 'text' not in col]
+
+        # Drop these columns from the DataFrame
+        pivot_df = pivot_df.drop(columns=columns_to_drop)
+        print(pivot_df.columns)
 
         # raw_schedule_df['text'] = raw_schedule_df.apply(lambda row: f"{row['subject']}\n{row['student_group']}\n{row['teacher']}\n[{row['schedule_id']}]", axis=1)
 
         # unique_rooms = raw_schedule_df['room'].unique()
-
 
         # # Convert start_time to a string format for MultiIndex compatibility
         # raw_schedule_df['start_time'] = raw_schedule_df['start_time'].apply(lambda t: t.strftime('%H:%M'))
@@ -59,4 +181,4 @@ class ScheduleManager:
         # df['day_int'] = df['day'].apply(lambda d: days_order.index(d))
         # df.sort_values(by=['day_int', 'start_time'], inplace=True)
         # df.drop('day_int', axis=1, inplace=True)
-        return df
+        return pivot_df.fillna('')  # df
